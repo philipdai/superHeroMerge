766f1a984e805726e26692e84bbb02b51ff3e89b
Philip Dai <smartielion@gmail.com>
2017-01-16 11:08:58 -0500

    Add models

---
+++ src/app/models/attributes.model.ts
@@ -0,0 +1,8 @@
+export class Attributes {
+  constructor(public intelligence: number = 0,
+              public strength: number = 0,
+              public speed: number = 0,
+              public durability: number = 0,
+              public power: number = 0,
+              public combat: number = 0) {}
+}
---
+++ src/app/models/hero.model.ts
@@ -0,0 +1,12 @@
+import { Attributes } from './attributes.model';
+import { Gender } from './gender.enum';
+
+export class Hero {
+  constructor(public id: number = -1,
+              public hero_name: string = "",
+              public real_name: string = "",
+              public gender: Gender = Gender.Male,
+              public attributes: Attributes = new Attributes(),
+              public powers: string[] = [],
+              public weaknesses: string[] = []) {}
+}
